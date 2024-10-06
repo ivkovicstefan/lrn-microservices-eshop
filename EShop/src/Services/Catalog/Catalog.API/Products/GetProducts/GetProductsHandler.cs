8f2cb5e2a5ce3b326d3b572056760f26061b2ec1
Stefan Ivković <ivkovics@outlook.com>
2024-10-06 09:52:42 +0200

    feat(catalog): add pagination to get products feature

--- EShop/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ EShop/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,6 @@
 ﻿namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
     internal class GetProductsQueryHandler
         (IDocumentSession documentSession)
@@ -9,7 +9,7 @@
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
             var products = await documentSession.Query<Product>()
-                .ToListAsync(cancellationToken);
+                .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
 
             return new GetProductsResult(products);
         }
