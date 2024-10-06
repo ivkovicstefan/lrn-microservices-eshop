a5bf0684566dda9cb95213e3886d19f3adc770cb
Stefan Ivković <ivkovics@outlook.com>
2024-10-06 09:37:43 +0200

    feat(catalog): add initial catalog database data

--- EShop/src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ EShop/src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -2,6 +2,7 @@
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Exceptions.Handlers;
 global using Carter;
+global using Catalog.API.Data;
 global using Catalog.API.Exceptions;
 global using Catalog.API.Models;
 global using FluentValidation;
