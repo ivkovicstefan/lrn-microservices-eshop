68ca5b755ed89e6e971fd6c8564fbd5e0521ec73
Stefan Ivković <ivkovics@outlook.com>
2024-10-07 22:23:39 +0200

    chore(basket): configure mediatr for dependency injection and http pipeline

--- EShop/src/Services/Basket/Basket.API/Program.cs
+++ EShop/src/Services/Basket/Basket.API/Program.cs
@@ -2,6 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Configure Services
 builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 
