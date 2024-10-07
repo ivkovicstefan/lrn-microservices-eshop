e9c3f0abcbaabe1390c3df3c7020d337c2dbb889
Stefan Ivković <ivkovics@outlook.com>
2024-10-07 22:19:53 +0200

    chore(basket): configure carter for dependency injection and http pipeline

--- EShop/src/Services/Basket/Basket.API/Program.cs
+++ EShop/src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure Services
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 // Configure HTTP pipeline
+app.MapCarter();
 
 app.Run();
