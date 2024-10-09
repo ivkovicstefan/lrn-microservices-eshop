4cb2b35ae5fba62d44e3e6fbadaf5130bf488d65
Stefan Ivković <ivkovics@outlook.com>
2024-10-09 14:37:43 +0200

    feat(discount): add discountdb context

---
+++ EShop/src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountDbContext : DbContext
+    {
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public DiscountDbContext(DbContextOptions<DiscountDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- EShop/src/Services/Discount/Discount.Grpc/Program.cs
+++ EShop/src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,10 +1,15 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-
+builder.Services.AddDbContext<DiscountDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DiscountDb"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
