a405ee75d1b8345b14a5f6efbb8f8b30b4f555c9
Stefan Ivković <ivkovics@outlook.com>
2024-10-10 19:46:58 +0200

    feat(discount): add get discount feature

--- EShop/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ EShop/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,12 +1,27 @@
-﻿using Grpc.Core;
+﻿using Discount.Grpc.Data;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services
 {
-    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    public class DiscountService
+        (DiscountDbContext discountDbContext, ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
     {
-        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
         {
-            return base.GetDiscount(request, context);
+            var coupon = await discountDbContext.Coupons
+                .FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+
+            if (coupon is null)
+            {
+                coupon = new() { ProductName = "No Discount", Amount = 0, Description = "No Discount" };
+            }
+
+            var response = coupon.Adapt<CouponModel>();
+
+            return response;
         }
 
         public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
