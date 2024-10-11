68fea46d253c34dd7c4ad276c37bbe243bf61909
Stefan Ivković <ivkovics@outlook.com>
2024-10-11 20:19:09 +0200

    feat(ordering): add dependency injection extension methods per layers

---
+++ EShop/src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            // TODO: Register Services
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            // TODO: Configure HTTP Pipeline
+            return app;
+        }
+    }
+}
---
+++ EShop/src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            // TODO: Register Services
+            return services;
+        }
+    }
+}
