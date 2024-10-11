381af53bb0a24d459d4a5f9ab5e398236c89a274
Stefan Ivković <ivkovics@outlook.com>
2024-10-11 18:31:48 +0200

    feat(basket): add discount deduction to store basket feature

--- EShop/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ EShop/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿using Discount.Grpc;
+
+namespace Basket.API.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string UserName);
@@ -15,16 +17,28 @@
     }
 
     internal class StoreBasketCommandHandler
-        (IBasketRepository basketRepository)
+        (IBasketRepository basketRepository,
+        DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
         : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
-            ShoppingCart cart = command.Cart;
+            await DeductDiscount(command.Cart, cancellationToken);
 
             await basketRepository.StoreBasket(command.Cart, cancellationToken);
 
             return new StoreBasketResult(command.Cart.UserName);
+        }
+
+        private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+        {
+            foreach (var item in cart.Items)
+            {
+                var coupon = await discountProtoServiceClient.GetDiscountAsync(
+                    new GetDiscountRequest() { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+
+                item.Price -= coupon.Amount;
+            }
         }
     }
 }
