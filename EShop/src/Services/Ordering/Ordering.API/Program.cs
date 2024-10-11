ffcecc108291dc0fb78af60e79c376b909fb64ff
Stefan Ivković <ivkovics@outlook.com>
2024-10-11 20:20:19 +0200

    feat(ordering): register dependency injection extension methods

--- EShop/src/Services/Ordering/Ordering.API/Program.cs
+++ EShop/src/Services/Ordering/Ordering.API/Program.cs
@@ -1,9 +1,18 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure Services
+builder.Services
+    .AddApiServices()
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure HTTP Pipeline
+app.UseApiServices();
 
 app.Run();
