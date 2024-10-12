6a19223e4aedec3fa784c89608ae44ca69e5278b
Stefan Ivković <ivkovics@outlook.com>
2024-10-12 18:44:16 +0200

    feat(ordering): add domain models

---
+++ EShop/src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record Payment
+    {
+        public string? CardName { get; } = default!;
+        public string CardNumber { get; } = default!;
+        public string? Expiration { get; } = default!;
+        public string CVV { get; set; } = default!;
+        public int PaymentMethod { get; set; }
+
+    }
+}
