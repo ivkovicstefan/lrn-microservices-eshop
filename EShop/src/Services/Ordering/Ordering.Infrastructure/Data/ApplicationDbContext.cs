e8c4f5e4a4536e29c2bf2d3833d2d6d5c1b127c9
Stefan Ivković <ivkovics@outlook.com>
2024-10-13 14:31:07 +0200

    feat(ordering): add custom db table configurations

--- EShop/src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ EShop/src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Ordering.Domain.Models;
+using System.Reflection;
 
 namespace Ordering.Infrastructure.Data
 {
@@ -14,5 +15,11 @@ namespace Ordering.Infrastructure.Data
         public DbSet<Product> Products => Set<Product>();
         public DbSet<Order> Orders => Set<Order>();
         public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
