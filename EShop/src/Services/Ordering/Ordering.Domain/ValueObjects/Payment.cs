8e1c0a984f1f4f4e143ab4e092d5c272b1fc35ca
Stefan Ivković <ivkovics@outlook.com>
2024-10-13 13:34:31 +0200

    feat(ordering): add domain logic to value objects

--- EShop/src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ EShop/src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -2,11 +2,33 @@
 {
     public record Payment
     {
+        private const int DefaultCvvLength = 3;
         public string? CardName { get; } = default!;
         public string CardNumber { get; } = default!;
         public string? Expiration { get; } = default!;
-        public string CVV { get; set; } = default!;
+        public string Cvv { get; set; } = default!;
         public int PaymentMethod { get; set; }
+        protected Payment() { }
 
+        private Payment(string cardName, string cardNumber, string expiration, string cvv,
+            int paymentMethod)
+        {
+            CardName = cardName;
+            CardNumber = cardNumber;
+            Expiration = expiration;
+            Cvv = cvv;
+            PaymentMethod = paymentMethod;
+        }
+
+        public static Payment Of(string cardName, string cardNumber, string expiration, string cvv,
+            int paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, DefaultCvvLength);
+
+            return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+        }
     }
 }
