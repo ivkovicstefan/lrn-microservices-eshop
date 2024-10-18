db2e0682fc989cc7f88b0ca997503c9ac31533a5
Stefan Ivković <ivkovics@outlook.com>
2024-10-18 15:07:53 +0200

    feat(ordering): add empty order domain event handlers

---
+++ EShop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain
+{
+    public class OrderCreatedEventHandler
+        (ILogger<OrderCreatedEventHandler> logger)
+        : INotificationHandler<OrderCreatedEvent>
+    {
+        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain event handled: {DomainEvent}",
+                notification.GetType().Name);
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EShop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderUpdatedEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain
+{
+    public class OrderUpdatedEventHandler
+        (ILogger<OrderUpdatedEventHandler> logger)
+        : INotificationHandler<OrderUpdatedEvent>
+    {
+        public Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain event handled: {DomainEvent}",
+                notification.GetType().Name);
+            throw new NotImplementedException();
+        }
+    }
+}
