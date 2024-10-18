b78d499bf1c3a311635af6da2586e1fe4f928a98
Stefan Ivković <ivkovics@outlook.com>
2024-10-18 18:10:17 +0200

    fix(ordering): add missing await to get orders by name endpoint

--- EShop/src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
+++ EShop/src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -7,14 +7,16 @@ using Ordering.Application.Orders.Queries.GetOrdersByName;
 namespace Ordering.API.Endpoints
 {
     //public record GetOrdersByNameRequest(string Name);
+
     public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+
     public class GetOrdersByName : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
             app.MapGet("/orders/{orderName}", async (string orderName, ISender sender) =>
             {
-                var result = sender.Send(new GetOrdersByNameQuery(orderName));
+                var result = await sender.Send(new GetOrdersByNameQuery(orderName));
                 var response = result.Adapt<GetOrdersByNameResponse>();
 
                 return Results.Ok(response);
