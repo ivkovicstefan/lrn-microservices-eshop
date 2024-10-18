4cf5bbcc30852a917a6bf6223192f0f60cd0497a
Stefan Ivković <ivkovics@outlook.com>
2024-10-18 16:35:35 +0200

    feat(ordering): add get orders by customer query

---
+++ EShop/src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+using Ordering.Application.Extensions;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public class GetOrdersByCustomerHandler
+        (IApplicationDbContext applicationDbContext)
+        : IRequestHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+    {
+        public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await applicationDbContext.Orders
+                .Include(o => o.OrderItems)
+                .Where(o => o.CustomerId == CustomerId.Of(query.CustomerId))
+                .OrderBy(o => o.OrderName)
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+        }
+    }
+}
