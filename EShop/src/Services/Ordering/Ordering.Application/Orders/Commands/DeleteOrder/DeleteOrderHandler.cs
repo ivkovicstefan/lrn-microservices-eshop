e6fae2e62113a914e48e527f8ee95ee161c3151d
Stefan Ivković <ivkovics@outlook.com>
2024-10-18 14:39:59 +0200

    feat(ordering): add delete order command

---
+++ EShop/src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.CQRS;
+using Ordering.Application.Data;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderHandler
+        (IApplicationDbContext applicationDbContext)
+        : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            var orderId = OrderId.Of(command.OrderId);
+            var order = await applicationDbContext.Orders
+                .FindAsync([orderId], cancellationToken: cancellationToken);
+
+            if (order is null)
+            {
+                throw new OrderNotFoundException(command.OrderId);
+            }
+
+            applicationDbContext.Orders.Remove(order);
+            await applicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return new DeleteOrderResult(true);
+        }
+    }
+}
