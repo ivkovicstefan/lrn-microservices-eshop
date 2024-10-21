21303b307de53d026f02161f37589cf7bdadbddc
Stefan Ivković <ivkovics@outlook.com>
2024-10-21 21:30:23 +0200

    chore(yarp-api-gateway): configure and register yarp reverse proxy to http pipeline

--- EShop/src/ApiGateways/YarpApiGateway/Program.cs
+++ EShop/src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure Services
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
 // Configure HTTP pipeline
+app.MapReverseProxy();
 
 app.Run();
