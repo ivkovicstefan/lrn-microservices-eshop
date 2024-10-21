e3f1a67e5e5aedf72ff18b91943f763eea5ff575
Stefan Ivković <ivkovics@outlook.com>
2024-10-21 22:19:02 +0200

    chore(yarp-api-gateway): configure and register rate limiter

--- EShop/src/ApiGateways/YarpApiGateway/Program.cs
+++ EShop/src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,12 +1,24 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure Services
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
 var app = builder.Build();
 
 // Configure HTTP pipeline
+app.UseRateLimiter();
 app.MapReverseProxy();
 
 app.Run();
